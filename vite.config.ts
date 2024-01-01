3bbb528b6e122975299f7f4f40db198d25b249d9
ZhangFeiqing <1083463827@qq.com>
2024-01-01 23:04:42 +0800

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
       '@': resolve(__dirname, './src'), // 设置 `@` 指向 `src` 目录
     },
   },
-  // vite默认会打包出umd和esmodule两种导出方式的文件，以下配置会打包出两份结果：
-  // gr-lib.umd.js umd导出方式，兼容amd commenjs
-  // gr-lib.mjs esmodule导出方式
   build: {
     lib: {
       entry: resolve(__dirname, './src/main.ts'),
